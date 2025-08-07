6e5e8f7d4fc77858505239522c08fb0f388ec09c
shomere <s.homere@alustudent.com>
2025-08-08 00:22:38 +0200

    icr

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // ✅ This is the key line to fix the blank page
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
